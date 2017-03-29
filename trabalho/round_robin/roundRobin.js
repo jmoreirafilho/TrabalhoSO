c5af9e4090dac53ff161b547441bfa36b35f4d92
Airton <airtonmfilho@hotmail.com>
2017-03-28 21:34:07 -0300

    Corrigindo logica para remover e adicionar nas filas do Round Robin

--- trabalho/round_robin/roundRobin.js
+++ trabalho/round_robin/roundRobin.js
@@ -10,30 +10,36 @@ app.factory('Scopes', function($rootScope) {
 		}
 	};
 });
+
 angular.module('view').controller('viewController', function ($scope, Scopes) {
+
+	// Define escopo de execução
+	Scopes.store('RoundRobin', $scope);
 
 	// Variaveis globais para parametros iniciais
 	var g_qtdNucleos = g_quantum = g_qtdProcsIniciais = 0;
 
 	// Variaveis globais para fator de cada fila de prioridade
-	var g_f0 = 9;
-	var g_f1 = 6;
-	var g_f2 = 3;
+	var g_f0 = 2.1;
+	var g_f1 = 1.8;
+	var g_f2 = 1.5;
+
+	// Indice da fila de prioridade que deve ser pegue
+	var g_indiceDaProximaFila = null;
+
+	var g_qtdMiss = 0;
 
 	// Fila de processos em execução (cores)
-	$scope.processosExecutando = [];
+	Scopes.get('RoundRobin').processosExecutando = [];
 	// Fila de processos aptos
-	$scope.processosAptos = {0: [], 1: [], 2: [], 3: []};
+	Scopes.get('RoundRobin').processosAptos = {0: [], 1: [], 2: [], 3: []};
 	// Fila de processos abortados
-	$scope.processosAbortados = [];
+	Scopes.get('RoundRobin').processosAbortados = [];
 	// Fila de processos finalizados
-	$scope.processosFinalizados = [];
-
-
-	Scopes.store('RoundRobin', $scope);
+	Scopes.get('RoundRobin').processosFinalizados = [];
 
 	// #2 - Método para inicializar escalonador
-	$scope.initRoundRobin = function (qtdNucleos, quantum, qtdProcsIniciais) {
+	Scopes.get('RoundRobin').initRoundRobin = function (qtdNucleos, quantum, qtdProcsIniciais) {
 
 		// Define as variaveis globais
 		g_qtdNucleos = qtdNucleos;
@@ -52,7 +58,7 @@ angular.module('view').controller('viewController', function ($scope, Scopes) {
 			var fila = Math.floor(Math.random() * 4);
 			
 			// Gera um randomico pra dizer a duração total do processo
-			var tempo = Math.floor(Math.random() * 100)+30;
+			var tempo = Math.floor(Math.random() * 10)+5;
 
 			// Pega o quantum do processo e multiplica ao valor de seu fator, dependendo de sua
 			// prioridade na fila de aptos
@@ -71,16 +77,16 @@ angular.module('view').controller('viewController', function ($scope, Scopes) {
 			}
 
 			// Adiciona processo na fila de aptos
-			$scope.processosAptos[fila].push({nome: "p"+i, fila: fila, quantum: Number(currentQuantum), tempo: tempo});
+			Scopes.get('RoundRobin').processosAptos[fila].push({nome: "p"+i, fila: fila, quantum: Number(currentQuantum), tempo: tempo});
 		}
 	}
 
 	var Processa = function(indice) {
-		var processo = $scope.processosExecutando[indice];
+		var processo = Scopes.get('RoundRobin').processosExecutando[indice];
 		var tempo = processo.quantum * 1000;
 
 		setTimeout(function () {
-			$scope.$apply(function () {
+			Scopes.get('RoundRobin').$apply(function () {
 				processo.tempo -= processo.quantum;							
 
 				if (processo.tempo <= 0) {
@@ -89,60 +95,70 @@ angular.module('view').controller('viewController', function ($scope, Scopes) {
 				}
 
 				if (processo.tempo > 0) {
-					$scope.processosAptos[processo.fila].push($scope.processosExecutando[indice]);
+					Scopes.get('RoundRobin').processosAptos[processo.fila].push(Scopes.get('RoundRobin').processosExecutando[indice]);
 				}
 
-				$scope.processosExecutando[indice] = null;	
+				Scopes.get('RoundRobin').processosExecutando[indice] = null;	
 
 			});
+
 			Processa.prototype.processaProximo(indice);
 		}, tempo);
 	}
 
 	Processa.prototype.processaProximo = function(indice) {
-		var filaDeAptos = 0;
-		//for (var nucleo = 0; nucleo < g_qtdNucleos; nucleo++) {
-			//if (filaDeAptos%4 === 0) {
-			//	filaDeAptos = 0;
-			//}
+		Processa.prototype.proximaFila();
 
-			// $scope.processosExecutando[nucleo] = $scope.processosAptos[filaDeAptos][0];
-			console.log(Scopes.get('RoundRobin').processosExecutando[indice]);
-			Scopes.get('RoundRobin').$apply(function () {
-				$scope.processosExecutando[indice] = $scope.processosAptos[3][0];
-				$scope.processosAptos[filaDeAptos].splice(0, 1);
-			});
+		Scopes.get('RoundRobin').$apply(function () {
+			Scopes.get('RoundRobin').processosExecutando[indice] = Scopes.get('RoundRobin').processosAptos[g_indiceDaProximaFila][0];
+			Scopes.get('RoundRobin').processosAptos[g_indiceDaProximaFila].splice(0, 1);
+		});
+
+		new Processa(indice);
 
 
-			new Processa(indice);
-
-			//}
-			//filaDeAptos++;
-		//}
 	};
 
+	Processa.prototype.proximaFila = function () {
+		if (g_indiceDaProximaFila == null) {
+			g_indiceDaProximaFila = 0;
+			return;
+		}
+
+		g_indiceDaProximaFila++;
+			
+		if (g_indiceDaProximaFila%4 == 0) {
+			g_indiceDaProximaFila = 0;
+		}
+
+		if (g_qtdMiss == 4) {
+			window.alert("Processo Finalizado!");
+		}
+
+		if (Scopes.get('RoundRobin').processosAptos[g_indiceDaProximaFila].length <= 0) {
+			Processa.prototype.proximaFila();
+			g_qtdMiss++;
+		}
+	}
+
 	// #3 - Método para iniciar processamento
-	$scope.iniciaProcessamento = function () {
-		var filaDeAptos = 0;
-		for (var nucleo = 0; nucleo < g_qtdNucleos; nucleo++) {
-			if (filaDeAptos%4 === 0) {
-				filaDeAptos = 0;
-			}
+	Scopes.get('RoundRobin').iniciaProcessamento = function () {
+		for (var indiceNucleo = 0; indiceNucleo < g_qtdNucleos; indiceNucleo++) {
+			if (Scopes.get('RoundRobin').processosExecutando[indiceNucleo] == null) {
 
-			if ($scope.processosExecutando[nucleo] == null) {
+				Processa.prototype.proximaFila();
 
-				$scope.processosExecutando[nucleo] = $scope.processosAptos[filaDeAptos][0];
-				$scope.processosAptos[filaDeAptos].splice(0, 1);
+				Scopes.get('RoundRobin').processosExecutando[indiceNucleo] = Scopes.get('RoundRobin').processosAptos[g_indiceDaProximaFila][0];
+				Scopes.get('RoundRobin').processosAptos[g_indiceDaProximaFila].splice(0, 1);
 
-				new Processa(nucleo, filaDeAptos);
+				new Processa(indiceNucleo);
 
 			}
-			filaDeAptos++;
 		}
 	}
 
 	// #4 - Método para adicionar processos em tempo de execução
-	$scope.adicionaProcesso = function () {
+	Scopes.get('RoundRobin').adicionaProcesso = function () {
 
 	}
 
@@ -150,5 +166,5 @@ angular.module('view').controller('viewController', function ($scope, Scopes) {
 	var p1 = new RegExp('[\?&]p1=([^&#]*)').exec(window.location.href)[1];
 	var p2 = new RegExp('[\?&]p2=([^&#]*)').exec(window.location.href)[1];
 	var p3 = new RegExp('[\?&]p3=([^&#]*)').exec(window.location.href)[1];
-	$scope.initRoundRobin(p1, p2, p3);
+	Scopes.get('RoundRobin').initRoundRobin(p1, p2, p3);
 });
